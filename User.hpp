01c34591318735f42d0f901302d7f2095db5e8e7
Jackson McGregor <jmcgreg@g.clemson.edu>
2018-09-27 17:35:41 -0400

    Update User.hpp

--- User.hpp
+++ User.hpp
@@ -5,22 +5,35 @@
 using namespace std;
 
 class User {
-		string username, password, email;
+		string name, username, password, email;
+		string address[10];
+		int addNum;
 		long phone;
 		int ID;
 	public:
 		User();
 		~User(); 
 		void registerUser();
+		string getName();
 		string getUsername();
 		string getPassword();
 		string getEmail();
+		string* getAddress();
+		
 		long getPhone();
 		int getID();
 
 		void setUsername(string i);
 		void setPassword(string i);
+		void setName(string i);
 		void setEmail(string i);
 		void setPhone(string i);
 		void setID(string i);
+		void setAddress(string* i, int j);
+		
+		void addAddress();
+		void removeAddress();
+		int getNumAddress();
+		
+		void changePassword();
 };
