904cafe7ba753a24c87b2beb55c2316127b60213
Jackson McGregor <jmcgreg@g.clemson.edu>
2018-09-27 17:37:05 -0400

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
 {
 	char regL;
 	cout << "Register (R) or Load (L)?" << endl;
+	bool add = false;
+	string addr[10];
+	int addrNum = 0;
 
 	cin >> regL;
 
@@ -35,6 +38,7 @@ int main(int argc, char *argv[])
 		ifstream in(buff);
 
 		string str;
+		string typeOfUser;
 
 		int i = 0;
 
@@ -42,31 +46,56 @@ int main(int argc, char *argv[])
 		{
 			if(i == 0)
 			{
-				d.setUsername(str);
+				typeOfUser = str;
 			}
-			if(i == 1)
+			if(typeOfUser == "Driver")
 			{
-				d.setPassword(str);
-			}
-			if(i == 2)
-			{
-				d.setEmail(str);
-			}
-			if(i == 3)
-			{
-				d.setPhone(str);
-			}
-			if(i == 4)
-			{
-				d.setID(str);
-			}
-			if(i == 5)
-			{
-				d.setSponsor(str);
-			}
-			if(i == 6)
-			{
-				d.setPoints(str);
+				if(i == 1)
+				{
+					d.setUsername(str);
+				}
+				if(i == 2)
+				{
+					d.setPassword(str);
+				}
+				if(i == 3)
+				{
+					d.setName(str);
+				}
+				if(i == 4)
+				{
+					d.setEmail(str);
+				}
+				if(i == 5)
+				{
+					d.setPhone(str);
+				}
+				if(i == 6)
+				{
+					d.setID(str);
+				}
+				if(i == 7)
+				{
+					d.setSponsor(str);
+				}
+				if(i == 8)
+				{
+					d.setPoints(str);
+				}
+				if(str == "ENDADDRESS")
+				{
+					add = false;
+					d.setAddress(addr,addrNum);
+				}
+				if(add == true)
+				{
+					addr[addrNum] = str;
+					addrNum++;
+				}
+				if(str == "ADDRESS")
+				{
+					add = true;
+				}
 			}
 			i++;
 		}
@@ -79,6 +108,7 @@ int main(int argc, char *argv[])
 	}
 
 	string t;
+	string* t2;
 	char input;
 	int i;
 	long l;
@@ -88,12 +118,18 @@ int main(int argc, char *argv[])
 
 	cout << "View Username (U)" << endl;
 	cout << "View Password (P)" << endl;
+	cout << "View Name (F)" << endl;
 	cout << "View Email (E)" << endl;
 	cout << "View Phone (H)" << endl;
 	cout << "View ID (I)" << endl;
 	cout << "View Sponsor (S)" << endl;
 	cout << "View Points (O)" << endl;
 	cout << "Change Points (C)" << endl;
+	cout << "Change Password (Q)" << endl;
+	cout << "View Address (V)" << endl;
+	cout << "Add Address (A)" << endl;
+	cout << "Remove Address (R)" << endl;
+	cout << "Update Name (N)" << endl;
 
 	while(x == 0)
 	{
@@ -104,6 +140,7 @@ int main(int argc, char *argv[])
 			t = d.getUsername();
 			cout <<	"Username: " << t << endl;
 		}
+
 		else if(input == 'P')
 		{
 			t = d.getPassword();
@@ -134,9 +171,40 @@ int main(int argc, char *argv[])
 			i = d.getPoints();
 			cout <<	"Points: " << i << endl;
 		}
-		else if(input = 'C')
+		else if(input == 'A')
+		{
+			d.addAddress();
+		}
+		else if(input == 'R')
+		{
+			d.removeAddress();
+		}
+		else if(input == 'F')
+		{
+			t= d.getName();
+			cout <<	"Name: " << t << endl;
+		}
+		else if(input == 'Q')
+		{
+			d.changePassword();
+		}
+		else if(input == 'C')
 		{
 			d.changePoints();
+		}
+		if(input == 'V')
+		{		
+			t2 = d.getAddress();
+			i = d.getNumAddress();
+			for(int o = 0; o < i; o++)
+			{
+				cout << "Address " << o << ": " << t2[o] << endl;
+			}
+		}
+		if(input == 'N')
+		{
+				cin >> t;
+				d.setUsername(t);
 		}
 		else
 		{
