4f96844126a940c5b9dca8a0e0b8aade7f2de3e1
Jackson McGregor <jmcgreg@g.clemson.edu>
2018-10-11 17:01:01 -0400

    Update AdminMain.cpp

--- AdminMain.cpp
+++ AdminMain.cpp
@@ -1,107 +1,426 @@
-#include "Admin.hpp"
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <dirent.h>
+#include <sys/types.h>
 
 using namespace std;
 
-Admin d;
+#include "Admin.h"
 
-int main(int argc, char *argv[])
+Admin::Admin()
 {
-	char regL;
-	cout << "Register (R) or Load (L)?" << endl;
-	bool add = false;
-	string addr[10];
-	int addrNum = 0;
 
-	cin >> regL;
+}
 
-	if(regL == 'R')
+Admin::~Admin()
+{
+}
+
+
+void Admin::registerAdmin()
+{
+	registerUser();
+}
+
+void Admin::registerDriver()
+{
+	Driver d;
+	d.registerDriver();
+	d.saveDriver();
+}
+
+void Admin::registerSponsor()
+{
+	Sponsor s;
+	s.registerSponsor();
+	s.saveSponsor();
+}
+
+void Admin::saveAdmin()
+{
+	ofstream myfile;
+	const char* file = getUsername().c_str();
+	myfile.open(file);
+	
+	myfile << "Admin" << "\n";
+
+	myfile << getUsername() << "\n";
+	myfile << getPassword() << "\n";
+	myfile << getName() << "\n";
+	myfile << getEmail() << "\n";
+	myfile << getPhone() << "\n";
+	myfile << getID() << "\n";	
+	myfile << "ADDRESS" << "\n";
+	
+	int n = getNumAddress();
+	string* addresses = getAddress();
+	for(int p = 0; p < n; p++)
 	{
-		d.registerAdmin();
-
-		cout << endl;
-
-		cout << "User registered." << endl;
-
-		cout << endl;
-
-		d.saveAdmin();
+		myfile << addresses[p] << "\n";
 	}
-	if(regL == 'L')
+	
+		myfile << "ENDADDRESS" << "\n";
+}
+
+void Admin::viewUser()
+{
+	string input;
+	cout << "Enter a user to view their info" << endl;
+	cin >> input;
+	bool yn = checkForUser(input);
+	
+	string str;
+	
+	bool add = true;
+	bool plate = true;
+	
+	ifstream in(input);
+	
+	if(yn == true)
 	{
-		string filename;
-
-		cout << "Choose a User" << endl;
-		cin >> filename;
-
-		const char *buff = filename.c_str();
-
-		ifstream in(buff);
-
-		string str;
-		string typeOfUser;
-
-		int i = 0;
-
-		while(getline(in, str))
+		int addrNum = 1;
+		int pltNum = 1;
+		getline(in, str);
+		if(str == "Driver")
 		{
-			if(i == 0)
-			{
-				typeOfUser = str;
-			}
-			if(typeOfUser == "Admin")
-			{
-				if(i == 1)
+				cout << "User type: Driver" << endl;
+				getline(in, str);
+				cout << "Username: " << str << endl;
+				getline(in, str);
+				cout << "Password: " << str << endl;
+				getline(in, str);
+				cout << "Real Name: " << str << endl;
+				getline(in, str);
+				cout << "E-Mail: " << str << endl;
+				getline(in, str);
+				cout << "Phone Number: " << str << endl;
+				getline(in, str);
+				cout << "ID: " << str << endl;
+				getline(in, str);
+				cout << "Sponsor: " << str << endl;
+				getline(in, str);
+				cout << "Points: " << str << endl;
+				getline(in, str);
+				cout << "License Number: " << str << endl;
+				getline(in, str);
+				while(add == true)
 				{
-					d.setUsername(str);
+					getline(in, str);
+					if(str == "ENDADDRESS")
+					{
+						add = false;
+					}
+					else
+					{	
+						cout << "Address "<< addrNum << ": " << str << endl;
+						addrNum++;
+					}
 				}
-				if(i == 2)
+				getline(in, str);
+				while(plate == true)
 				{
-					d.setPassword(str);
-				}
-				if(i == 3)
+					getline(in, str);
+					if(str == "ENDPLATES")
+					{
+						plate = false;
+					}
+					else
+					{	
+						cout << "Plate "<< pltNum << ": " << str << endl;
+						pltNum++;
+					}
+				}				
+		}
+		else if(str == "Sponsor")
+		{
+			cout << "User type: Driver" << endl;
+				getline(in, str);
+				cout << "Username: " << str << endl;
+				getline(in, str);
+				cout << "Password: " << str << endl;
+				getline(in, str);
+				cout << "Real Name: " << str << endl;
+				getline(in, str);
+				cout << "E-Mail: " << str << endl;
+				getline(in, str);
+				cout << "Phone Number: " << str << endl;
+				getline(in, str);
+				cout << "ID: " << str << endl;
+				getline(in, str);
+				while(add == true)
 				{
-					d.setName(str);
+					getline(in, str);
+					if(str == "ENDADDRESS")
+					{
+						add = false;
+					}
+					else
+					{	
+						cout << "Address "<< addrNum << ": " << str << endl;
+						addrNum++;
+					}
 				}
-				if(i == 4)
+				getline(in, str);
+				while(plate == true)
 				{
-					d.setEmail(str);
-				}
-				if(i == 5)
+					getline(in, str);
+					if(str == "ENDSPONS")
+					{
+						plate = false;
+					}
+					else
+					{	
+						cout << "Driver "<< pltNum << ": " << str << endl;
+						pltNum++;
+					}
+				}				
+		}
+		else if(str == "Admin")
+		{
+				cout << "User type: Admin" << endl;
+				getline(in, str);
+				cout << "Username: " << str << endl;
+				getline(in, str);
+				cout << "Password: " << str << endl;
+				getline(in, str);
+				cout << "Real Name: " << str << endl;
+				getline(in, str);
+				cout << "E-Mail: " << str << endl;
+				getline(in, str);
+				cout << "Phone Number: " << str << endl;
+				getline(in, str);
+				cout << "ID: " << str << endl;
+				getline(in, str);
+				while(add == true)
 				{
-					d.setPhone(str);
+					getline(in, str);
+					if(str == "ENDADDRESS")
+					{
+						add = false;
+					}
+					else
+					{	
+						cout << "Address "<< addrNum << ": " << str << endl;
+						addrNum++;
+					}
 				}
-				if(i == 6)
-				{
-					d.setID(str);
-				}
-				if(str == "ENDADDRESS")
-				{
-					add = false;
-					d.setAddress(addr,addrNum);
-				}
-				if(add == true)
-				{
-					addr[addrNum] = str;
-					addrNum++;
-				}
-				if(str == "ADDRESS")
-				{
-					add = true;
-				}
-			}
-			i++;
+		}
+		else
+		{
+			cout << "Something went wrong..." << endl;
+		}
 	}
-	
-	string t;
-	string* t2;
+}
 
-	char input;
-	int* i2;
-		
-	long l;
-	int x = 0;
+bool Admin::checkForUser(string name) 
+{
+	string str;
+	ifstream in(name);
 	
-	d.viewUser();
+    if (FILE *file = fopen(name.c_str(), "r")) 
+	{
+		cout << "User found." << endl;
+		return true;
+    } 
+	else 
+	{
+		cout << "User not found." << endl;
+        return false;
+    }   
+}
+
+void Admin::listOfFiles()
+{
+   struct dirent *entry;
+   DIR *dir = opendir(".");
+   if (dir == NULL) {
+      return;
+   }
+   while ((entry = readdir(dir)) != NULL) {
+   cout << entry->d_name << endl;
+   }
+   closedir(dir);
+}
+
+void Admin::viewAllDrivers()
+{
+	struct dirent *entry;
+   DIR *dir = opendir(".");
+   if (dir == NULL) {
+      return;
+   }
+   while ((entry = readdir(dir)) != NULL) 
+   {
+   		string name = entry->d_name;
+		ifstream in(name);
+  		if(FILE *file = fopen(name.c_str(), "r"))
+		{
+				string str;
+				getline(in, str);
+				if(str == "Driver")
+				{
+					cout << entry->d_name << endl;
+				}
+		}
+   }
+   closedir(dir);
+}
+
+void Admin::viewAllSponsors()
+{
+		struct dirent *entry;
+   DIR *dir = opendir(".");
+   if (dir == NULL) {
+      return;
+   }
+   while ((entry = readdir(dir)) != NULL) 
+   {
+   		string name = entry->d_name;
+		ifstream in(name);
+  		if(FILE *file = fopen(name.c_str(), "r"))
+		{
+				string str;
+				getline(in, str);
+				if(str == "Sponsor")
+				{
+					cout << entry->d_name << endl;
+				}
+		}
+   }
+   closedir(dir);
+}
+
+void Admin::viewAllAdmins()
+{
+			struct dirent *entry;
+   DIR *dir = opendir(".");
+   if (dir == NULL) {
+      return;
+   }
+   while ((entry = readdir(dir)) != NULL) 
+   {
+   		string name = entry->d_name;
+		ifstream in(name);
+  		if(FILE *file = fopen(name.c_str(), "r"))
+		{
+				string str;
+				getline(in, str);
+				if(str == "Admin")
+				{
+					cout << entry->d_name << endl;
+				}
+		}
+   }
+   closedir(dir);
+}
+
+void Admin::deleteUser()
+{
+	cout << "Type in user you want to delete" << endl;
+	string name, str;
+	cin >> name;
+	
+	ifstream in(name);
+	if(FILE *file = fopen(name.c_str(), "r"))
+		{
+				getline(in, str);
+				if(str == "Admin")
+				{
+					cout << name << " is an admin" << endl;
+					cout << "Are you sure you want to delete this user? (Y/N)" << endl;
+					char yn;
+					cin >> yn;
+					if(yn = 'Y')
+					{
+					if( remove(name.c_str()) != 0 )
+					{
+    						perror( "Error deleting file" );
+					}
+ 				 	else
+				 	{
+    						puts( "File successfully deleted" );
+					}
+					}
+				}
+				else if(str == "Driver")
+				{
+					cout << name << " is a driver" << endl;
+					cout << "Are you sure you want to delete this user? (Y/N)" << endl;
+					char yn;
+					cin >> yn;
+					if(yn = 'Y')
+					{
+					if( remove(name.c_str()) != 0 )
+					{
+    						perror( "Error deleting file" );
+					}
+ 				 	else
+				 	{
+    						puts( "File successfully deleted" );
+					}
+					}
+				}
+				else if(str == "Sponsor")
+				{
+					cout << name << " is a sponsor" << endl;
+					cout << "Are you sure you want to delete this user? (Y/N)" << endl;
+					char yn;
+					cin >> yn;
+					if(yn = 'Y')
+					{
+					if( remove(name.c_str()) != 0 )
+					{
+    						perror( "Error deleting file" );
+					}
+ 				 	else
+				 	{
+    						puts( "File successfully deleted" );
+					}
+					}
+				}
+				else
+				{
+					cout << "File found, but an error occured" << endl;
+				}
+		}
+		else
+		{
+			cout << "File not found" << endl;
+		}
+}
+
+void Admin::editUser()
+{
+	cout << "Which user's info do you want to edit?" << endl;
+	string user;
+	cin >> user;
+	ifstream in(user);
+	string str;
+	if(FILE *file = fopen(user.c_str(), "r"))
+		{
+				getline(in, str);
+				if(str == "Driver")
+				{
+					cout << "Driver " << user << " found" << endl;
+					Sponsor s;
+					Driver d = s.setDriver(user);
+					editDriver(d);
+				}
+		}
 	
 }
+
+void Admin::editDriver(Driver name)
+{
+	cout << "Choose aspect to edit" << endl;
+	cout << "Password (P)" << endl;	
+	char v;
+	cin >> v;
+	if(v == 'P')
+	{
+		name.changePassword();
+		name.saveDriver();
+	}
 }
