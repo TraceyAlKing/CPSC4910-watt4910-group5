a7287ea74dbb1e9b5070de618a54d6d4f366e556
Jackson McGregor <jmcgreg@g.clemson.edu>
2018-11-15 17:45:46 -0500

    driver cin/cout
    removed

--- Driver.hpp
+++ Driver.hpp
@@ -35,7 +35,7 @@ class Driver: public User {
 		Driver();
 		~Driver(); 
 		
-		void registerDriver();
+                void registerDriver(string us, string ps, string nm, string em, long ph, string ad, int ln, string lp);
 		
 		int* getPoints();
 		int getPoints(int sid);
@@ -56,9 +56,9 @@ class Driver: public User {
 		void setLNum(int i);
 		
 		void updateLNum();
-		void addLP();
-		void removeLP();
-		void viewLP();
+                void addLP(int i);
+                void removeLP(int i);
+                int* viewLP();
 		
 		int getLNumNum();
 		void setLPNum(string* i, int j);
