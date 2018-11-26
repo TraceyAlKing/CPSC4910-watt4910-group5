20891743aa3e09dfd3bc019bb0908394083b14aa
Jackson McGregor <jmcgreg@g.clemson.edu>
2018-11-26 10:22:24 -0500

    cout/cin admin

--- Admin.cpp
+++ Admin.cpp
@@ -89,7 +89,7 @@ void Admin::updateAdmin(){
 	db().updateAdmin(std::to_string(getID()), getName(), getEmail(), getPassword(), std::to_string(getPhone()));
 }
 
-void Admin::viewUser()
+/*void Admin::viewUser()
 {
 	string input;
 	cout << "Enter a user to view their info" << endl;
@@ -233,7 +233,7 @@ void Admin::viewUser()
 			cout << "Something went wrong..." << endl;
 		}
 	}
-}
+}*/
 
 bool Admin::checkForUser(string name) 
 {
@@ -403,11 +403,8 @@ void Admin::viewAllAdmins()
    closedir(dir);
 }
 
-void Admin::deleteUser()
+void Admin::deleteUser(string name)
 {
-	cout << "Type in user you want to delete" << endl;
-	string name, str;
-	cin >> name;
 	
 	ifstream in(name);
 	if(FILE *file = fopen(name.c_str(), "r"))
@@ -415,12 +412,6 @@ void Admin::deleteUser()
 				getline(in, str);
 				if(str == "Admin")
 				{
-					cout << name << " is an admin" << endl;
-					cout << "Are you sure you want to delete this user? (Y/N)" << endl;
-					char yn;
-					cin >> yn;
-					if(yn == 'Y')
-					{
 					if( remove(name.c_str()) != 0 )
 					{
     						perror( "Error deleting file" );
@@ -433,12 +424,6 @@ void Admin::deleteUser()
 				}
 				else if(str == "Driver")
 				{
-					cout << name << " is a driver" << endl;
-					cout << "Are you sure you want to delete this user? (Y/N)" << endl;
-					char yn;
-					cin >> yn;
-					if(yn == 'Y')
-					{
 					if( remove(name.c_str()) != 0 )
 					{
     						perror( "Error deleting file" );
@@ -446,17 +431,10 @@ void Admin::deleteUser()
  				 	else
 				 	{
     						puts( "File successfully deleted" );
-					}
 					}
 				}
 				else if(str == "Sponsor")
 				{
-					cout << name << " is a sponsor" << endl;
-					cout << "Are you sure you want to delete this user? (Y/N)" << endl;
-					char yn;
-					cin >> yn;
-					if(yn == 'Y')
-					{
 					if( remove(name.c_str()) != 0 )
 					{
     						perror( "Error deleting file" );
@@ -465,17 +443,9 @@ void Admin::deleteUser()
 				 	{
     						puts( "File successfully deleted" );
 					}
-					}
-				}
-				else
-				{
-					cout << "File found, but an error occured" << endl;
 				}
 		}
-		else
-		{
-			cout << "File not found" << endl;
-		}
+
 }
 /* @TODO: add this code back, errors compiling
 void Admin::editUser()
